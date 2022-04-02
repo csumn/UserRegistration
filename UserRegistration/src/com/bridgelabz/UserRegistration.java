ee8e7dcb0b81cc78501d9c1b160e033f677e6299
csumn <csumn13@gmail.com>
2022-04-02 09:11:52 +0530

    Last name starts with Cap and has minimum 3 characters

--- UserRegistration/src/com/bridgelabz/UserRegistration.java
+++ UserRegistration/src/com/bridgelabz/UserRegistration.java
@@ -9,17 +9,29 @@ public class UserRegistration {
 		System.out.println("Enter first name\n");
 		Scanner scanner = new Scanner(System.in);
 		String firstName = scanner.nextLine();
-		Boolean check = validateFirstName(firstName);
-		if (check) {
+		System.out.println("Enter last name\n");
+		String lastName = scanner.nextLine();
+		Boolean firstNameCheck = validateFirstName(firstName);
+		if (firstNameCheck) {
 			System.out.println("Valid first name");
 		} else
 			System.out.println("Invalid first name");
+		Boolean lastNameCheck = validateLastname(lastName);
+		if (lastNameCheck) {
+			System.out.println("Valid last name");
+		} else
+			System.out.println("Invalid last name");
 		scanner.close();
 	}
 
 	private static Boolean validateFirstName(String firstName) {
 		String matcher = "^[A-Z]{1}[a-z]{2,}$";
 		Boolean check = Pattern.matches(matcher, firstName);
+		return check;
+	}
+	private static Boolean validateLastname(String lastName) {
+		String lastNameMatcher = "^[A-Z]{1}[a-z]{2,}$";
+		Boolean check = Pattern.matches(lastNameMatcher, lastName);
 		return check;
 	}
 }
